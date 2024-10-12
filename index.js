01c197346db3024dc6aa1f0ca80428c8486afb35
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-10-12 16:16:56 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    name: "Hamza",
+    streetAdress: "KilimaniRd"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return{...employee, [key]: value};
+}
+const newEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Ali");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+const newEmployee2 = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "rhaptaRd")
+
+console.log(newEmployee2);
+console.log(employee);
+console.log(newEmployee);
+
+function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: omitted, ...newEmployee } = employee; // Use destructuring to omit the key
+    return newEmployee; // Return the new object without the specified key
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+
+
+
+
